8fc9c38ab1047a236d78a8c6e4deedd3b46379cd
mariokancha <marioekancha2013@gmail.com>
2020-02-21 20:43:19 -0300

    Projeto Criado com H2 funcionando

---
+++ src/main/java/com/sipagbackend/SipagbackendApplication.java
@@ -0,0 +1,13 @@
+package com.sipagbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipagbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipagbackendApplication.class, args);
+	}
+
+}
