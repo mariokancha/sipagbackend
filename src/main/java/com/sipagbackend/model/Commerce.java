5b83153de9f28a168b1ebe0c9102752c3f6a9338
mariokancha <marioekancha2013@gmail.com>
2020-02-24 14:11:08 -0300

    alterando a entidade commerce

--- src/main/java/com/sipagbackend/model/Commerce.java
+++ src/main/java/com/sipagbackend/model/Commerce.java
@@ -2,45 +2,23 @@ package com.sipagbackend.model;
 
 import java.io.Serializable;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
+
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CNPJ;
 
 @Entity
 @Table(name = "COMMERCE")
 public class Commerce implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	@Embedded
-	@NotNull
-	@Valid
-	@AttributeOverrides(value = { @AttributeOverride(name = "cep", column = @Column(name = "CEP")), 
-			@AttributeOverride(name = "logradouro", column = @Column(name = "LOGRADOURO" )),
-			@AttributeOverride(name = "bairro", column = @Column(name = "BAIRRO")),
-			@AttributeOverride(name = "cidade", column = @Column(name = "CIDADE" )),
-			@AttributeOverride(name = "uf", column = @Column(name = "UF" ))
-	})
-	private CommerceEndereco endereco;
-
-	@Embedded
-	@NotNull
-	@Valid
-	@AttributeOverrides(value = {
-			@AttributeOverride(name = "foneCelular", column = @Column(name = "FONE_CELULAR")),
-			@AttributeOverride(name = "foneComercial", column = @Column(name = "FONE_COMERCIAL")),
-			@AttributeOverride(name = "foneResidencial", column = @Column(name = "FONE_RESIDENCIAL")),
-			@AttributeOverride(name = "email", column = @Column(name = "EMAIL")) })
-	private CommerceContato contato;
 
 	@Id
 	@Column(name = "IDCOMMERCE")
@@ -51,11 +29,29 @@ public class Commerce implements Serializable {
 	@NotNull
 	@Size(min = 20, max = 100)
 	private String nome;
-
+	
 	@Column(name = "CNPJ")
 	@Size(min = 14, max = 14)
 	@NotNull
+	@CNPJ
 	private String cnpj;
+	@NotNull
+	private String cep;
+	@NotNull
+	private String logradouro;
+	@NotNull
+	private String bairro;
+	@NotNull
+	private String cidade;
+	@NotNull
+	private String uf;
+	private String complemento;
+	private String foneResidencial;
+	private String foneComercial;
+	@NotNull
+	private String foneCelular;
+	@NotNull
+	private String email;
 
 	public Commerce() {
 
@@ -85,20 +81,84 @@ public class Commerce implements Serializable {
 		this.cnpj = cnpj;
 	}
 
-	public CommerceEndereco getEndereco() {
-		return endereco;
+	public String getCep() {
+		return cep;
 	}
 
-	public void setEndereco(CommerceEndereco endereco) {
-		this.endereco = endereco;
+	public void setCep(String cep) {
+		this.cep = cep;
 	}
 
-	public CommerceContato getContato() {
-		return contato;
+	public String getLogradouro() {
+		return logradouro;
 	}
 
-	public void setContato(CommerceContato contato) {
-		this.contato = contato;
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getFoneResidencial() {
+		return foneResidencial;
+	}
+
+	public void setFoneResidencial(String foneResidencial) {
+		this.foneResidencial = foneResidencial;
+	}
+
+	public String getFoneComercial() {
+		return foneComercial;
+	}
+
+	public void setFoneComercial(String foneComercial) {
+		this.foneComercial = foneComercial;
+	}
+
+	public String getFoneCelular() {
+		return foneCelular;
+	}
+
+	public void setFoneCelular(String foneCelular) {
+		this.foneCelular = foneCelular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public static long getSerialversionuid() {
@@ -126,5 +186,15 @@ public class Commerce implements Serializable {
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		return "Commerce [idCommerce=" + idCommerce + ", nome=" + nome + ", cnpj=" + cnpj + ", cep=" + cep
+				+ ", logradouro=" + logradouro + ", bairro=" + bairro + ", cidade=" + cidade + ", uf=" + uf
+				+ ", complemento=" + complemento + ", foneResidencial=" + foneResidencial + ", foneComercial="
+				+ foneComercial + ", foneCelular=" + foneCelular + ", email=" + email + "]";
+	}
+	
+	
 
 }
